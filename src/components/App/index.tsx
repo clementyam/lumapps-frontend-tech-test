0601ffe7b1808cc06d35b18379f0cd917c6443aa
Juani Galán <juan@lumapps.com>
2025-10-16 14:09:05 +0200

    chore(test): first react render

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// src/App.tsx
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, Vite + React + TypeScript! 🚀</h1>
+      <p>This component is using TSX.</p>
+    </div>
+  );
+}
